ea075388e3a2c6f6e519d551ff88be2007aa9b80
Ilia Volkov <volkov.ilya@gmail.com>
2011-09-28 23:10:55 +0400

    Test check-in

---
+++ TrackInfo 2.m
@@ -0,0 +1,81 @@
+#import "TrackInfo.h"
+#import "iTunes.h"
+#import "SearchLyricsResult.h"
+
+@implementation TrackInfo
+
+@synthesize id;
+@synthesize name;
+@synthesize artist;
+@synthesize album;
+@synthesize lyrics;
+@synthesize artwork;
+
++ (TrackInfo*)trackInfoWithiTunesTrack:(iTunesTrack*)track {
+    return [[TrackInfo alloc] initWithiTunesTrack:track];
+}
+
+- (void)syncWithSearchResult:(SearchLyricsResult*)searchResult {
+    [self syncWithSearchResult:searchResult overridingExistingValues:false];
+}
+
+- (void)syncWithSearchResult:(SearchLyricsResult*)searchResult overridingExistingValues:(BOOL)override {
+    if (searchResult == nil)
+        return;
+        
+    if ([self.lyrics length] == 0 || override)
+        self.lyrics = searchResult.lyrics;
+}
+
+- (BOOL)isEqualToTrackInfo:(TrackInfo *)track {
+    if (track == nil)
+        return false;
+    
+    return [self.id isEqualToNumber:track.id];
+}
+
+- (NSString*)handleNil:(NSString*)string {
+    return string != nil ? string : [NSString string];
+}
+
+- (id)initWithiTunesTrack:(iTunesTrack *)track {
+    self = [super init];
+    if (self != nil) {
+        self.id = [NSNumber numberWithUnsignedInteger:track.databaseID];
+        self.name = [self handleNil:track.name];
+        self.artist = [self handleNil:track.artist];
+        self.album = [self handleNil:track.album];
+        self.lyrics = [self handleNil:track.lyrics];
+        
+        NSString *name = self.name;
+        NSString *artist = self.artist;
+        
+        if ([[track artworks] count] > 0)
+            self.artwork = [(iTunesArtwork*)[[track artworks] objectAtIndex:0] data];
+                
+        internalTrack = track;
+    }
+    
+    return self;
+}
+
+- (void)update {
+    if (![internalTrack exists])
+        return;
+    
+    if(![internalTrack.lyrics isEqualToString:self.lyrics]) {
+        internalTrack.lyrics = self.lyrics == nil ? [NSString string] : self.lyrics;
+    }
+}
+
+- (void)saveState {
+    lyricsSnapshot = self.lyrics;
+}
+
+- (void)restoreState {    
+    self.lyrics = lyricsSnapshot;
+    self.artist = internalTrack.artist;
+    self.name = internalTrack.name;
+}
+
+@end
